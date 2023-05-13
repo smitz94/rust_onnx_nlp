880e3eefed4c5814e828c6c6fbbf034d1dff2b1d
smit zaveri <smitzaveri12@gmail.com>
2023-05-13 17:21:41 +0530

    added command run

--- src/bin/inference_api.rs
+++ src/bin/inference_api.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, web, App, HttpServer, Result, Responder};
 use serde::{Deserialize, Serialize};
+use std::process::Command;
 
 #[derive(Deserialize)]
 struct RequestBody {
@@ -14,8 +15,13 @@ struct ResponseBody {
 #[get("/infer")]
 async fn inference(req_body: web::Json<RequestBody>) -> Result<impl Responder> {
     let text: &str = &req_body.text;
+    let output = Command::new(".../target/release/rust_onnx_nlp")
+                         .arg(text).output().expect("failed to execute process.");
+    
+    let prediction = String::from_utf8(output.stdout).unwrap();
+
     let res_body = ResponseBody{
-        text: text.to_string(),
+        text: prediction,
     };
      Ok(web::Json(res_body))
 }
